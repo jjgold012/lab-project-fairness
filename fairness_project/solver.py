9e89a7d7887ec267f86707580d891fb7e167c0a6
beng <ben.goldberger@innovid.com>
2018-01-07 09:47:00 +0200

    fix the rate axis in plot

--- fairness_project/solver.py
+++ fairness_project/solver.py
@@ -6,10 +6,6 @@ from sklearn.model_selection import train_test_split
 from fairness_project.fairness_data import *
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
 def plot_results(subplot, results, type):
     weights = [r['weight'] for r in results]
     acc = [r['measures']['acc'] for r in results]
@@ -17,12 +13,16 @@ def plot_results(subplot, results, type):
     fpr_diff = [r['measures']['fpr_diff'] for r in results]
     r_fnr_diff = [r['results']['fnr_diff'] for r in results]
     r_fpr_diff = [r['results']['fpr_diff'] for r in results]
+    subplot.set_autoscaley_on(False)
+    subplot.set_ylim([0, 1])
     subplot.set_title(type)
     subplot.plot(weights, acc, 'r-', label="Accuracy", linewidth=2)
     subplot.plot(weights, fnr_diff, 'g-', label="fnr diff", linewidth=2)
     subplot.plot(weights, r_fnr_diff, 'g--', label="relaxed fnr diff", linewidth=2)
     subplot.plot(weights, fpr_diff, 'b-', label="fpr diff", linewidth=2)
     subplot.plot(weights, r_fpr_diff, 'b--', label="relaxed fpr diff", linewidth=2)
+    subplot.set_xlabel('Fairness weight')
+    subplot.set_ylabel('Rate')
     subplot.legend(loc='best', prop={'size':11}, ncol=1)
 
 
@@ -40,6 +40,32 @@ def show_results(results_squared, results_abs):
     plt.show()
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def measure_results(x_test, y_test, protected_index, w, fp, fn):
+    y_hat = np.round(sigmoid(np.dot(x_test, w)))
+    y_1, y_0 = np.array(split_by_protected_value(x_test, y_test, protected_index))[[1, 3]]
+    y_1_hat, y_0_hat = np.array(split_by_protected_value(x_test, y_hat, protected_index))[[1, 3]]
+
+    all_measures = measures(y_test, y_hat)
+    _1_measures = measures(y_1, y_1_hat)
+    _0_measures = measures(y_0, y_0_hat)
+    fpr_diff = abs(_1_measures['fpr'] - _0_measures['fpr'])
+    fnr_diff = abs(_1_measures['fnr'] - _0_measures['fnr'])
+    return {
+        'acc': all_measures['acc'],
+        '1_fpr': _1_measures['fpr'],
+        '1_fnr': _1_measures['fnr'],
+        '0_fpr': _0_measures['fpr'],
+        '0_fnr': _0_measures['fnr'],
+        'fpr_diff': fpr_diff,
+        'fnr_diff': fnr_diff,
+        'objective': all_measures['acc'] + (fpr_diff if fp else 0) + (fnr_diff if fn else 0)
+    }
+
+
 def solve_convex(x, y, protected_index, gamma, fp_weight, fn_weight, squared=True):
     x_1, y_1, x_0, y_0 = split_by_protected_value(x, y, protected_index)
     x_1_pos = get_positive_examples(x_1, y_1)
@@ -52,7 +78,7 @@ def solve_convex(x, y, protected_index, gamma, fp_weight, fn_weight, squared=Tru
     diff_neg = (np.sum(x_1_neg, axis=0)/x_1_neg.shape[0]) - (np.sum(x_0_neg, axis=0)/x_0_neg.shape[0])
 
     log_likelihood = (y.T*(x*w) - cp.sum_entries(cp.logistic(x*w)))
-    
+
     if squared:
         fpr_diff = \
             cp.square(diff_neg*w)
@@ -83,28 +109,6 @@ def solve_convex(x, y, protected_index, gamma, fp_weight, fn_weight, squared=Tru
     }
 
 
-def measure_results(x_test, y_test, protected_index, w, fp, fn):
-    y_hat = np.round(sigmoid(np.dot(x_test, w)))
-    y_1, y_0 = np.array(split_by_protected_value(x_test, y_test, protected_index))[[1, 3]]
-    y_1_hat, y_0_hat = np.array(split_by_protected_value(x_test, y_hat, protected_index))[[1, 3]]
-
-    all_measures = measures(y_test, y_hat)
-    _1_measures = measures(y_1, y_1_hat)
-    _0_measures = measures(y_0, y_0_hat)
-    fpr_diff = abs(_1_measures['fpr'] - _0_measures['fpr'])
-    fnr_diff = abs(_1_measures['fnr'] - _0_measures['fnr'])
-    return {
-        'acc': all_measures['acc'],
-        '1_fpr': _1_measures['fpr'],
-        '1_fnr': _1_measures['fnr'],
-        '0_fpr': _0_measures['fpr'],
-        '0_fnr': _0_measures['fnr'],
-        'fpr_diff': fpr_diff,
-        'fnr_diff': fnr_diff,
-        'objective': all_measures['acc'] + (fpr_diff if fp else 0) + (fnr_diff if fn else 0)
-    }
-
-
 def fairness(problem: FairnessProblem):
     x = problem.X
     y = problem.Y
@@ -124,12 +128,10 @@ def fairness(problem: FairnessProblem):
             temp_res_abs = list()
             for j in range(5): #TODO check for cross validation sklearn
                 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=j)
-
                 conv_squared = solve_convex(x_train, y_train, protected_index, gamma, fp_weight=fp_weight, fn_weight=fn_weight, squared=True)
                 conv_abs = solve_convex(x_train, y_train, protected_index, gamma, fp_weight=fp_weight, fn_weight=fn_weight, squared=False)
                 measures_squared = measure_results(x_test, y_test, protected_index, conv_squared['w'], fp=problem.fp, fn=problem.fn)
                 measures_abs = measure_results(x_test, y_test, protected_index, conv_abs['w'], fp=problem.fp, fn=problem.fn)
-
                 temp_res_squared.append({'results': conv_squared, 'measures': measures_squared})
                 temp_res_abs.append({'results': conv_abs, 'measures': measures_abs})
 
