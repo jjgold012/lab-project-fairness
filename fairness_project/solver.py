dd7a7c8bb06e9a8e1ea21b41c22b33c0cccea79a
beng <ben.goldberger@innovid.com>
2018-01-07 20:34:05 +0200

    fix cross validation

--- fairness_project/solver.py
+++ fairness_project/solver.py
@@ -8,11 +8,11 @@ from fairness_project.fairness_data import *
 
 def plot_results(subplot, results, type):
     weights = [r['weight'] for r in results]
-    acc = [r['measures']['acc'] for r in results]
-    fnr_diff = [r['measures']['fnr_diff'] for r in results]
-    fpr_diff = [r['measures']['fpr_diff'] for r in results]
-    r_fnr_diff = [r['results']['fnr_diff'] for r in results]
-    r_fpr_diff = [r['results']['fpr_diff'] for r in results]
+    acc = [r['test_measures']['acc'] for r in results]
+    fnr_diff = [r['test_measures']['fnr_diff'] for r in results]
+    fpr_diff = [r['test_measures']['fpr_diff'] for r in results]
+    r_fnr_diff = [r['test_results']['fnr_diff'] for r in results]
+    r_fpr_diff = [r['test_results']['fpr_diff'] for r in results]
     subplot.set_autoscaley_on(False)
     subplot.set_ylim([0, 1])
     subplot.set_title(type)
@@ -44,7 +44,7 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def measure_results(x_test, y_test, protected_index, w, fp, fn):
+def measure_objective_results(x_test, y_test, protected_index, w, fp, fn):
     y_hat = np.round(sigmoid(np.dot(x_test, w)))
     y_1, y_0 = np.array(split_by_protected_value(x_test, y_test, protected_index))[[1, 3]]
     y_1_hat, y_0_hat = np.array(split_by_protected_value(x_test, y_hat, protected_index))[[1, 3]]
@@ -61,8 +61,35 @@ def measure_results(x_test, y_test, protected_index, w, fp, fn):
         '0_fpr': _0_measures['fpr'],
         '0_fnr': _0_measures['fnr'],
         'fpr_diff': fpr_diff,
+        'fnr_diff': fnr_diff
+    }
+
+
+def logistic(x):
+    return np.log(1 + np.exp(x))
+
+
+def measure_relaxed_results(x_test, y_test, protected_index, w, fp, fn, fp_weight, fn_weight, squared=True):
+    x_1, y_1, x_0, y_0 = split_by_protected_value(x_test, y_test, protected_index)
+    x_1_pos = get_positive_examples(x_1, y_1)
+    x_1_neg = get_negative_examples(x_1, y_1)
+    x_0_pos = get_positive_examples(x_0, y_0)
+    x_0_neg = get_negative_examples(x_0, y_0)
+    diff_pos = (np.sum(x_1_pos, axis=0)/x_1_pos.shape[0]) - (np.sum(x_0_pos, axis=0)/x_0_pos.shape[0])
+    diff_neg = (np.sum(x_1_neg, axis=0)/x_1_neg.shape[0]) - (np.sum(x_0_neg, axis=0)/x_0_neg.shape[0])
+    log_likelihood = (y_test.T.dot(x_test.dot(w)) - np.sum(logistic(x_test.dot(w))))
+    if squared:
+        fpr_diff = np.square(diff_neg.dot(w))
+        fnr_diff = np.square(diff_pos.dot(w))
+    else:
+        fpr_diff = np.abs(diff_neg.dot(w))
+        fnr_diff = np.abs(diff_pos.dot(w))
+    w_norm_square = np.sum(np.square(w))
+    return {
+        'll': log_likelihood,
         'fnr_diff': fnr_diff,
-        'objective': all_measures['acc'] + (fpr_diff if fp else 0) + (fnr_diff if fn else 0)
+        'fpr_diff': fpr_diff,
+        'objective': -log_likelihood + (fp_weight*fpr_diff if fp else 0) + (fn_weight*fnr_diff if fn else 0)
     }
 
 
@@ -80,15 +107,11 @@ def solve_convex(x, y, protected_index, gamma, fp_weight, fn_weight, squared=Tru
     log_likelihood = (y.T*(x*w) - cp.sum_entries(cp.logistic(x*w)))
 
     if squared:
-        fpr_diff = \
-            cp.square(diff_neg*w)
-        fnr_diff = \
-            cp.square(diff_pos*w)
+        fpr_diff = cp.square(diff_neg*w)
+        fnr_diff = cp.square(diff_pos*w)
     else:
-        fpr_diff = \
-            cp.abs(diff_neg*w)
-        fnr_diff = \
-            cp.abs(diff_pos*w)
+        fpr_diff = cp.abs(diff_neg*w)
+        fnr_diff = cp.abs(diff_pos*w)
 
     w_norm_square = cp.sum_squares(w)
 
@@ -110,10 +133,10 @@ def solve_convex(x, y, protected_index, gamma, fp_weight, fn_weight, squared=Tru
 
 
 def fairness(problem: FairnessProblem):
+    print(problem.description)
     x = problem.X
     y = problem.Y
     protected_index = problem.protected_index
-    print(problem.description)
     results_squared = list()
     results_abs = list()
     for weight in np.linspace(problem.weight_gt, problem.weight_lt, num=problem.weight_res):
@@ -128,12 +151,18 @@ def fairness(problem: FairnessProblem):
             temp_res_abs = list()
             for j in range(5): #TODO check for cross validation sklearn
                 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=j)
+
                 conv_squared = solve_convex(x_train, y_train, protected_index, gamma, fp_weight=fp_weight, fn_weight=fn_weight, squared=True)
                 conv_abs = solve_convex(x_train, y_train, protected_index, gamma, fp_weight=fp_weight, fn_weight=fn_weight, squared=False)
-                measures_squared = measure_results(x_test, y_test, protected_index, conv_squared['w'], fp=problem.fp, fn=problem.fn)
-                measures_abs = measure_results(x_test, y_test, protected_index, conv_abs['w'], fp=problem.fp, fn=problem.fn)
-                temp_res_squared.append({'results': conv_squared, 'measures': measures_squared})
-                temp_res_abs.append({'results': conv_abs, 'measures': measures_abs})
+
+                relaxed_squared = measure_relaxed_results(x_test, y_test, protected_index, conv_squared['w'], fp=problem.fp, fn=problem.fn, fp_weight=fp_weight, fn_weight=fn_weight, squared=True)
+                relaxed_abs = measure_relaxed_results(x_test, y_test, protected_index, conv_abs['w'], fp=problem.fp, fn=problem.fn, fp_weight=fp_weight, fn_weight=fn_weight, squared=False)
+
+                measures_squared = measure_objective_results(x_test, y_test, protected_index, conv_squared['w'], fp=problem.fp, fn=problem.fn)
+                measures_abs = measure_objective_results(x_test, y_test, protected_index, conv_abs['w'], fp=problem.fp, fn=problem.fn)
+
+                temp_res_squared.append({'train_results': conv_squared, 'test_results': relaxed_squared, 'test_measures': measures_squared})
+                temp_res_abs.append({'train_results': conv_abs, 'test_results': relaxed_abs, 'test_measures': measures_abs})
 
             gamma_result_squared = {'gamma': gamma}
             gamma_result_abs = {'gamma': gamma}
@@ -147,8 +176,8 @@ def fairness(problem: FairnessProblem):
             res_squared.append(gamma_result_squared)
             res_abs.append(gamma_result_abs)
 
-        best_squared = res_squared[np.array([r['measures']['objective'] for r in res_squared]).argmin()]
-        best_abs = res_abs[np.array([r['measures']['objective'] for r in res_abs]).argmin()]
+        best_squared = res_squared[np.array([r['test_results']['objective'] for r in res_squared]).argmin()]
+        best_abs = res_abs[np.array([r['test_results']['objective'] for r in res_abs]).argmin()]
         best_squared['weight'] = weight
         best_abs['weight'] = weight
         results_squared.append(best_squared)
